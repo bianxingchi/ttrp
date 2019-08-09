5e1ba6f021ac996e208d0c7ddbd0fbbfbd6aef06
Sean Bian <starunbian@foxmail.com>
2019-08-09 15:32:23 +0800

    Update ttrp.py

--- ttrp.py
+++ ttrp.py
@@ -1,13 +1,3 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-主函数来调用其他的模块，
-这里到底要怎么写呢？
-像示例代码那样，把目标函数、约束、变量都显式定义出来？
-
-d_ij 是需要计算出来的一个变量；
-cplex 的回调函数到底怎么使用？
-distance callback
-demand callback
-capacity constraints
-solution printer
